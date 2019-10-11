67c96d90154b602f65b78c6c2cf93ad4fbd3f8af
Oguzhan Korucu <99058978@mydavinci.nl>
2019-10-11 12:07:17 +0200

    upddate

---
+++ forloop.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <script>
+    for (var i = 0; i <= 10; i++) {
+    
+        for (var y = 1; y <= 10; y++) {
+            console.log(i * y + x);
+        }
+    }
+}
+    
+    </script>
+</html>
